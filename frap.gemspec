97ac86207c62ac480b10cdfa8113fb09984eedf2
James Gascoigne - Taylor <james@flippakitten.com>
2019-07-18 12:56:39 +0200

    move createapp to it's own class

--- frap.gemspec
+++ frap.gemspec
@@ -1,4 +1,3 @@
-
 lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require "frap/version"
@@ -33,7 +32,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
   spec.required_ruby_version = '>= 2.6.1'
 
-  spec.add_development_dependency "bundler", "~> 2.0"
+  spec.add_development_dependency "bundler", ">= 1.17.3"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
 end
--- lib/frap.rb
+++ lib/frap.rb
@@ -1,27 +1,5 @@
-require "frap/version"
+require 'frap/version'
+require 'frap/create_app'
 
 module Frap
-  class Error < StandardError; end
-
-  class CreateApp
-    attr_accessor :name
-
-    def initialize(name)
-      @name = name.downcase
-    end
-    def build
-      puts "1 ================ Create Parent Directory #{name}"
-      system("mkdir #{name}")
-      Dir.chdir(name)
-      puts "2 ================ Rails new --api #{name}_server"
-      system("rails new #{name}_server")
-      puts "3 ================ Flutter create app #{name}_ui"
-      system("flutter create #{name}_ui")
-      puts '4 ================ Setup BLoC pattern using Slidy'
-      Dir.chdir("#{name}_ui")
-      system('rm -rf lib/')
-      system('mkdir lib')
-      system('slidy start')
-    end
-  end
 end
