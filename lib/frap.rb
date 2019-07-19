c4bfc04f5ce55a1da910a50466f7bece313802a9
James Gascoigne - Taylor <james@flippakitten.com>
2019-07-19 15:37:57 +0200

    add Thor

--- lib/frap.rb
+++ lib/frap.rb
@@ -1,5 +1,31 @@
 require 'frap/version'
 require 'frap/create_app'
+require 'frap/generators/generate'
+require 'frap/generators/config'
+require 'thor'
+require 'yaml'
 
 module Frap
+  class CLI < Thor
+    desc 'new APP_NAME', 'Lower cased underscored app name'
+    long_desc <<-LONGDESC
+      Using the "app_name", it will create the parent directory and the Rails and Flutter sub directories, 
+      "app_name_server" and "app_name_ui"
+    LONGDESC
+    def new(app_name)
+      CreateApp.new(app_name).build
+    end
+
+    desc 'generate GENERATOR RESOURCE_NAME', 'Specify the generator and a name'
+    long_desc <<-LONGDESC
+      Generate all the boiler plate code such as controllers, pages and blocks for Rails and Flutter.
+      run `frap g help` to see a list of existing generators
+    LONGDESC
+    subcommand 'generate', Frap::Generators::Generate
+
+    desc 'temp NAME', 'does something temporary'
+    def temp(name)
+      Frap::Generators::Config.new([name]).invoke_all
+    end
+  end
 end
