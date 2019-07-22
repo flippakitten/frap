0d1f8890b26960ed8970d696d331b7fdd9dbb36a
James Gascoigne - Taylor <james@flippakitten.com>
2019-07-22 07:37:19 +0200

    cleanup

--- lib/frap/create_app.rb
+++ lib/frap/create_app.rb
@@ -11,10 +11,10 @@ module Frap
 
     def build
       setup_parent_directory
-      # create_rails_api_server
-      # create_flutter_app
-      # setup_bloc_patter
-      # show_completed_notes
+      create_rails_api_server
+      create_flutter_app
+      setup_bloc_pattern
+      show_completed_notes
     end
 
     private
@@ -22,34 +22,40 @@ module Frap
     def setup_parent_directory
       puts "1 ================ Create Parent Directory #{name}"
 
-      system("mkdir #{name}")
-      Dir.chdir(name)
-      Frap::Generators::Config.new([name]).invoke_all
+      gen = Frap::Generators::Config.new([name])
+      gen.destination_root = working_dir
+      gen.invoke_all
     end
 
     def create_rails_api_server
       puts "2 ================ Rails new #{name}_server --api"
+      Dir.chdir(working_dir)
 
       system("rails new #{name}_server --api -T")
     end
 
     def create_flutter_app
       puts "3 ================ Flutter create app #{name}_ui"
+      Dir.chdir(working_dir)
       system("flutter create #{name}_ui")
     end
 
-    def setup_bloc_patter
+    def setup_bloc_pattern
       puts '4 ================ Setup BLoC pattern using Slidy'
-      Dir.chdir("#{name}_ui")
+      Dir.chdir("#{working_dir}/#{name}_ui")
       system('rm -rf lib/')
       system('mkdir lib')
       system('slidy start')
-      Dir.chdir('../')
+      Dir.chdir(working_dir)
     end
 
     def show_completed_notes
-      puts '================================= '
+      puts '================================='
       puts 'TODO: Install your favourite Rails Testing library'
+    end
+
+    def working_dir
+      @working_dir ||= "#{Dir.pwd}/#{name}"
     end
   end
 end
