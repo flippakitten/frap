b64fb162f42e3e74502d6caa12d47668ef5606bc
James Gascoigne - Taylor <james@flippakitten.com>
2019-08-09 15:54:23 +0200

    add resource fixtures

---
+++ spec/fixtures/flutter/create_resource_spec/with_post_and_blog_resource/resources/blog_api_provider.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:frap_test_suite_app_ui/src/models/blogs.dart';
+import 'package:frap_test_suite_app_ui/src/constants/app_constants.dart';
+
+class BlogApiProvider {
+  Future<Blogs> fetchAllBlogs() async {
+    final response =
+    await http.get("${RootUrl}/blogs");
+
+    if (response.statusCode == 200) {
+      return Blogs.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load Blogs');
+    }
+  }
+}
---
+++ spec/fixtures/flutter/create_resource_spec/with_post_resource/constants/routing.dart
@@ -0,0 +1,4 @@
+
+const String HomeScreenRoute = '/';
+const String PostIndexScreenRoute = '/posts'; 
+const String PostShowScreenRoute = '/post';
