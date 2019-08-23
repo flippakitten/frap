f02c0ce6f8b07e182b69c25c89ef799cdf1e03a2
James Gascoigne - Taylor <james@flippakitten.com>
2019-08-23 11:41:35 +0200

    use new templates in new app generation

--- lib/frap/generators/flutter_config.rb
+++ lib/frap/generators/flutter_config.rb
@@ -5,10 +5,10 @@ module Frap
 
       desc 'Generate Flutter Config files'
       argument :name
-      argument :folder_name
+      argument :flutter_app_name
 
       def self.source_root
-        File.dirname(__FILE__) + '/templates/dart'
+        File.dirname(__FILE__) + '/templates/flutter'
       end
 
       def configure_directories
@@ -18,73 +18,57 @@ module Frap
       end
 
       def create_directories
-        empty_directory("#{lib_directory}/src/animations")
-        empty_directory("#{lib_directory}/src/blocs")
-        empty_directory("#{lib_directory}/src/models")
-        empty_directory("#{lib_directory}/src/resources")
-        empty_directory("#{lib_directory}/src/config")
-        empty_directory("#{lib_directory}/src/constants")
-        empty_directory("#{lib_directory}/src/screens")
-        empty_directory("#{lib_directory}/src/widgets")
+        empty_directory("#{lib_directory}/animations")
+        empty_directory("#{lib_directory}/bloc")
+        empty_directory("#{lib_directory}/config")
+        empty_directory("#{lib_directory}/config/constants")
+        empty_directory("#{lib_directory}/models")
+        empty_directory("#{lib_directory}/repositories")
+        empty_directory("#{lib_directory}/screens")
+        empty_directory("#{lib_directory}/widgets")
+
         invoke :create_files
       end
 
       def create_files
+        @name_capitalized_plural = name.capitalize.pluralize
+        @name_capitalized_single = name.capitalize.singularize
+        @name_downcased_plural   = name.downcase.pluralize
+        @name_downcased_single   = name.downcase.singularize
+
         template('main.dart.erb', "#{lib_directory}/main.dart")
-        template('src/constants/app_constants.dart.erb', "#{src_directory}/constants/app_constants.dart")
-        template('src/resources/repository.dart.erb', "#{src_directory}/resources/repository.dart")
-        template('src/app.dart.erb', "#{src_directory}/app.dart")
-        template('src/animations/slide_right_route.dart.erb', "#{src_directory}/animations/slide_right_route.dart")
-        template('src/config/router.dart.erb', "#{src_directory}/config/router.dart")
-        template('src/constants/pages_list.dart.erb', "#{src_directory}/constants/pages_list.dart")
-        template('src/constants/routing.dart.erb', "#{src_directory}/constants/routing.dart")
-        template('src/screens/base.dart.erb', "#{src_directory}/screens/home_screen.dart")
-        template('src/screens/undefined_route_screen.dart.erb', "#{src_directory}/screens/undefined_route_screen.dart")
-        template('src/widgets/base_app_bar.dart.erb', "#{src_directory}/widgets/base_app_bar.dart")
-        template('src/widgets/popup_menu_widget.dart.erb', "#{src_directory}/widgets/popup_menu_widget.dart")
-
-        invoke :setup_pages
-      end
-
-      def setup_pages
-        inject_into_file("#{src_directory}/config/router.dart", home_router, after: /^.*switch \(settings.name\) \{$/)
-        inject_into_file("#{src_directory}/constants/pages_list.dart", home_route, after: /const List<Page> pages = const <Page>\[.*$/)
-        append_to_file("#{src_directory}/constants/routing.dart", "const String HomeScreenRoute = '/';\n")
+        template('app.dart.erb', "#{lib_directory}/app.dart")
+        template('animations/slide_right_route.dart.erb', "#{lib_directory}/animations/slide_right_route.dart")
+        template('bloc/bloc.dart.erb', "#{lib_directory}/bloc/bloc.dart")
+        template('bloc/simple_bloc_delegate.dart.erb', "#{lib_directory}/bloc/simple_bloc_delegate.dart")
+        template('config/constants/app_constants.dart.erb', "#{lib_directory}/config/constants/app_constants.dart")
+        template('config/constants/pages_list.dart.erb', "#{lib_directory}/config/constants/pages_list.dart")
+        template('config/constants/routing.dart.erb', "#{lib_directory}/config/constants/routing.dart")
+        template('config/router.dart.erb', "#{lib_directory}/config/router.dart")
+        template('models/models.dart.erb', "#{lib_directory}/models/models.dart")
+        template('repositories/repositories.dart.erb', "#{lib_directory}/repositories/repositories.dart")
+        template('screens/home_screen.dart.erb', "#{lib_directory}/screens/home_screen.dart")
+        template('screens/undefined_route_screen.dart.erb', "#{lib_directory}/screens/undefined_route_screen.dart")
+        template('widgets/base_app_bar.dart.erb', "#{lib_directory}/widgets/base_app_bar.dart")
+        template('widgets/popup_menu_widget.dart.erb', "#{lib_directory}/widgets/popup_menu_widget.dart")
       end
 
       private
 
       def working_directory
-        "#{destination_root}/#{folder_name}"
+        "#{destination_root}/#{flutter_app_name}"
       end
 
       def lib_directory
         "#{working_directory}/lib"
       end
 
-      def src_directory
-        "#{lib_directory}/src"
-      end
-
       def pub_files
         <<~EOF.chomp
           \n  dio: ^2.1.13
             rxdart: ^0.22.0
-            bloc_pattern: ^2.2.3
             http: ^0.12.0+2
-        EOF
-      end
-
-      def home_router
-        <<~EOF.chomp
-          \n    case HomeScreenRoute:
-                return SlideRightRoute(widget:HomeScreen());
-        EOF
-      end
-
-      def home_route
-        <<~EOF.chomp
-          \n  const Page(title: 'Home', icon: Icons.home, route: HomeScreenRoute),
+            flutter_bloc: ^0.20.1
         EOF
       end
     end
